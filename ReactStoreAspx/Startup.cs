15b454abf3e877361d67a11c82ee79f5154f98f3
jcssll <essentialnetworker@gmail.com>
2019-05-05 17:35:30 -0400

    Add project files.

---
+++ ReactStoreAspx/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ReactStoreAspx.Startup))]
+namespace ReactStoreAspx
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
