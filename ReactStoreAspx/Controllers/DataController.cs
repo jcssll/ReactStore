b8efc641184ab9545ee9bc286b528057e3304598
jcssll <essentialnetworker@gmail.com>
2019-05-10 14:37:06 -0400

    created a rest api controller with 3 main endpoints

---
+++ ReactStoreAspx/Controllers/DataController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ReactStoreAspx.Controllers
+{
+    public class DataController : Controller
+    {
+
+        //Creating 3 endpoints
+        // GET: Data
+        [HttpGet]
+        public ActionResult GetMenuList()
+        {
+            return null;
+        }
+
+        [HttpGet]
+        public string GetUserId()
+        {
+            return "1";
+        }
+
+        //place order: when someone places an order react app will talk to the server in json format
+        [HttpPost]
+        public ActionResult PlaceOrder()
+        {
+            return null;
+        }
+    }
+}
