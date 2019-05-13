d03ab79d983d4d7fb21e855e42899f21f3c3fa25
jcssll <essentialnetworker@gmail.com>
2019-05-13 01:29:01 -0400

    API endpoint notes and setup

--- ReactStoreAspx/Controllers/DataController.cs
+++ ReactStoreAspx/Controllers/DataController.cs
@@ -8,22 +8,24 @@ namespace ReactStoreAspx.Controllers
 {
     public class DataController : Controller
     {
+        //Goal : Create 3 endpoints
 
-        //Creating 3 endpoints
+        //1. We need our food items so we need to get to our fooditem menu list
         // GET: Data
         [HttpGet]
         public ActionResult GetMenuList()
         {
             return null;
         }
-
+        //2. We will need the react app to know the user id when the user signs in 
         [HttpGet]
         public string GetUserId()
         {
             return "1";
         }
 
-        //place order: when someone places an order react app will talk to the server in json format
+        //3. place order:  When user places an order the react app will send those orders in json format and talk to the server through this endpoint
+        //user will post order data 
         [HttpPost]
         public ActionResult PlaceOrder()
         {
