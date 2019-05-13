2692f9cea3ca18084ce3be87159bfcb8679b0765
jcssll <essentialnetworker@gmail.com>
2019-05-13 01:49:55 -0400

    testing GetMenuList endpoint

--- ReactStoreAspx/Controllers/DataController.cs
+++ ReactStoreAspx/Controllers/DataController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ReactStoreAspx.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,21 +11,34 @@ namespace ReactStoreAspx.Controllers
     {
         //Goal : Create 3 endpoints
 
-        //1. We need our food items so we need to get to our fooditem menu list
+        // I want to return the GetMenuList in JSON format
+        //1. Declare a variable that will hold the food items
+        //this is in the namespace model
+
+        public IList<FoodItem> menuItems;
+        //(First Endpoint): We need our food items so we need to get to our fooditem menu list
         // GET: Data
         [HttpGet]
         public ActionResult GetMenuList()
         {
-            return null;
+            menuItems = new List<FoodItem>();
+            using (var db = new AppDbContext())
+            {
+                foreach(var f in db.FoodItems)
+                {
+                    menuItems.Add(f);
+                }
+            }
+            return Json(menuItems, JsonRequestBehavior.AllowGet);
         }
-        //2. We will need the react app to know the user id when the user signs in 
+        //(Second Endpoint) We will need the react app to know the user id when the user signs in 
         [HttpGet]
         public string GetUserId()
         {
             return "1";
         }
 
-        //3. place order:  When user places an order the react app will send those orders in json format and talk to the server through this endpoint
+        //(Third Endpoint) place order:  When user places an order the react app will send those orders in json format and talk to the server through this endpoint
         //user will post order data 
         [HttpPost]
         public ActionResult PlaceOrder()
