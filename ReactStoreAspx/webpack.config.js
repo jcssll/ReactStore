cb35a3aaf5bf1de3bcd0f0a4e5d4079ec5fee9e6
jcssll <essentialnetworker@gmail.com>
2019-05-17 03:03:11 -0400

    Added the ts json and the webpack config js

---
+++ ReactStoreAspx/webpack.config.js
@@ -0,0 +1,24 @@
+﻿//---webpack.config.js---------------------
+
+module.exports = {
+    entry: "./Content/src/food.tsx",
+    mode: "development",
+    output: {
+        filename: "bundle.js"
+    },
+    resolve: {
+        extensions: ['.ts', '.js', '.tsx', '.jsx']
+    },
+
+    devtool: 'source-map',
+
+    // Add the loader for .ts files.
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: 'awesome-typescript-loader'
+            }
+        ]
+    }
+};
