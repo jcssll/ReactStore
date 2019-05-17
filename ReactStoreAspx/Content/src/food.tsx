ac3c7fa339aa9514aba46f06ccf56be4987e6cef
jcssll <essentialnetworker@gmail.com>
2019-05-17 02:36:40 -0400

    created typescript file ts

---
+++ ReactStoreAspx/Content/src/food.tsx
@@ -0,0 +1,10 @@
+﻿//test code
+
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+ReactDOM.render(
+    <div>
+        <h2>Test</h2>
+    </div>,
+    document.getElementById("foodorder")
+);
