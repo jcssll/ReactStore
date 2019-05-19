e88150bd4ab858c7f9e9f94bb019b2e65ba45030
jcssll <essentialnetworker@gmail.com>
2019-05-19 13:14:08 -0400

    uninstalled and reinstalled webpack created new react homepage and created img folder

---
+++ ReactStoreAspx/Content/src/food.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const React = require("react");
+const ReactDOM = require("react-dom");
+ReactDOM.render(React.createElement("div", null,
+    React.createElement("h2", null, "Test")), document.getElementById("foodorder"));
+//# sourceMappingURL=food.js.map
