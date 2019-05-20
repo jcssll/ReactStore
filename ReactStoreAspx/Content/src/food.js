cdaf842448bf19096ab8cf9e14a3191220e624a2
jcssll <essentialnetworker@gmail.com>
2019-05-20 11:18:54 -0400

    included tsx file and tsconfig

--- ReactStoreAspx/Content/src/food.js
+++ ReactStoreAspx/Content/src/food.js
@@ -3,5 +3,5 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const React = require("react");
 const ReactDOM = require("react-dom");
 ReactDOM.render(React.createElement("div", null,
-    React.createElement("h2", null, "Test")), document.getElementById("foodorder"));
+    React.createElement("h2", null, "Test123")), document.getElementById("foodorder"));
 //# sourceMappingURL=food.js.map
--- ReactStoreAspx/Content/src/food.tsx
+++ ReactStoreAspx/Content/src/food.tsx
@@ -4,7 +4,7 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 ReactDOM.render(
     <div>
-        <h2>Test</h2>
+        <h2>Test123</h2>
     </div>,
     document.getElementById("foodorder")
 );
