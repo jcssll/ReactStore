5054a91333631935cb782210fa30d5b0d754ce6c
jcssll <essentialnetworker@gmail.com>
2019-05-21 00:03:32 -0400

    added models

---
+++ ReactStoreAspx/Content/src/Models.tsx
@@ -0,0 +1,15 @@
+﻿export interface FoodModel {
+    Id: number;
+    Description: string;
+    Picture: number;
+    Price: number;
+    Quantity: number;
+}
+
+export interface IAppStat {
+    items: FoodModel[];
+    myOrder: FoodModel[];
+    showPopup: boolean;
+    userId: number;
+    orderPlaced:boolean;
+}
